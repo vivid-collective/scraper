c06e67910d1c05fcff60ee494c441d25e17c833a
jeremy.clayton.91@gmail.com <jeremy.clayton.91@gmail.com>
2018-01-23 19:28:08 -0700

    did first layer of scrape

--- scrape.js
+++ scrape.js
@@ -6,29 +6,53 @@ const beautify = require("json-beautify");
 
 const outputPath = 'sunglasses.json';
 const port = 8080;
-var url = 'http://www.dragonalliance.com/en-us/';
+var url = 'https://www.smithoptics.com/us/Root/Men%27s/Sunglasses/New/c/1110';
 allSunglassLinks = [];
 
-// request(url, function (err, resp, body) {
-//     let $ = cheerio.load(body);
-//     var newUrl = $('#productnav').attr('href');
-//     nextLayer(newUrl);
-// });
 
-nextLayer()
-function nextLayer(newUrl) {
-    request('https://us.vonzipper.com/shop/sunglasses-polarized', function (err, resp, body) {
+function start() {
+    request(url, function (err, resp, body) {
         let $ = cheerio.load(body);
-        // let mynew = $('#site-container');
-        // // console.log(mynew.children());
-        // let thing = mynew.children().first().next().next();
-        // console.log("the next layer after main is " + thing)
-        // var next = thing.children().first();
-        // console.log(next.children())
-        console.log($('.product-name').text())
-
+        $('.inner_product_panel').each(function () {
+            let data = $(this)
+            let newObj = {}
+            newObj.link = data.find('a').first().attr('href');
+            // newObj.model = data.find('h4').first().text();
+            // console.log(newObj)
+            allSunglassLinks.push(newObj);
+            // console.log(allSunglassLinks)
+        })
+        nextLevel(allSunglassLinks)
     });
 }
 
-app.listen(port);
-console.info(`Server running at ${port}`);
+function nextLevel(arr) {
+    console.log(arr);
+        arr.forEach(element => {
+            request(element.link, function (err, resp, body) {
+                let $ = cheerio.load(body);
+                let modal = $('#sidebar > h1').text()
+                console.log(modal);
+            })
+        });
+    }
+    start();
+
+
+    // nextLayer()
+    // function nextLayer(newUrl) {
+    //     request('https://us.vonzipper.com/shop/sunglasses-polarized', function (err, resp, body) {
+    //         let $ = cheerio.load(body);
+    //         // let mynew = $('#site-container');
+    //         // // console.log(mynew.children());
+    //         // let thing = mynew.children().first().next().next();
+    //         // console.log("the next layer after main is " + thing)
+    //         // var next = thing.children().first();
+    //         // console.log(next.children())
+    //         console.log($('.product-name').text())
+
+    //     });
+    // }
+
+    app.listen(port);
+    console.info(`Server running at ${port}`);
